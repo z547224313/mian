eedbf162366741aec2c60950895769a11744ff8c
zhangpengLaptop <547224313@qq.com>
2021-09-02 09:24:15 +0800

    ZJ FAIL

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
-function deep(arr) {
-  
+function A() {
+  this.b = 3;
 }
+var b = 5;
+A.prototype = {
+  b: 7,
+  d: 2,
+  log:()=> {
+    console.log(2222);
+  }
+};
+var c = new A();
+A();
+
+console.log(b);
+console.log(c.b);
+console.log(c.log()); 
